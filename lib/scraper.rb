3fe1fb86bb70e96d2f4e772981e89aad160085fa
emi kojima <emi8emi@gmail.com>
2018-04-17 17:26:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,37 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+    doc = Nokogiri::HTML(open(index_url))
+
+    student_hash = {}
+    students = []
+      doc.css(".student-card").each do |x|
+      student_hash = {
+        :name => x.css("h4").text,
+        :location => x.css("p").text,
+        :profile_url => x.css("a").attr("href").value
+      }
+       students << student_hash
+        end
+      #   :student_profile => doc.css(".student-card a").each {|element| element["href"]}.compact
+      # }
+      students
+    end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    student_profile = {}
+    binding.pry
+     student_profile = {
+       :twitter=> doc.css(".social-icon-container a").attr("href").value,
+       :linkedin=> doc.css(".social-icon-container a")[1]['href'],
+       :github=> doc.css(".social-icon-container a")[2]['href'],
+       :blog=> doc.css(".social-icon-container a")[3]['href'], 
+    #   :profile_quote=> doc.css (".profile-quote").text,
+       :bio=> doc.css(".description-holder").text.gsub(/\s+/, " ").strip
+     }
     
+    student_profile 
   end
 
 end
-
