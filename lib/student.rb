0516bc9d9987ae39913a3982d4ca468ac0df72e9
emi kojima <emi8emi@gmail.com>
2018-04-17 20:13:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -10,7 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    binding.pry
   end
 
   def add_student_attributes(attributes_hash)
