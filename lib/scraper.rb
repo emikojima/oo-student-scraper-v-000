ebe1c90eb9d83b64911a03a59aee3c1574d4f1f0
emi kojima <emi8emi@gmail.com>
2018-04-17 17:38:01 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,7 +31,7 @@ class Scraper
        :github=> doc.css(".social-icon-container a")[2]['href'],
        :blog=> doc.css(".social-icon-container a")[3]['href'], 
     #   :profile_quote=> doc.css (".profile-quote").text,
-       :bio=> doc.css(".description-holder").text.gsub(/\s+/, " ").strip
+       :bio=> doc.css(".description-holder p").text.gsub(/\s+/, " ").strip
      }
     
     student_profile 
